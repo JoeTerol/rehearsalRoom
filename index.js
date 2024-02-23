c4dce61c86f738f422f989fb97db073dbca83479
JoeTerol <teroljoseeduardo@gmail.com>
2024-02-22 22:28:22 -0300

    feat: products routes async

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const express = require('express');
-const app = express();
-const port = 3001;
+const routerApi = require('./routes');
 
-app.get('/', (req, res)=> {
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
   res.send('hola mi server en express');
 });
-app.get('/nuevaruta', (req,res) => {
-  res.send('Hola, soy una nueva ruta');
-});
-app.get('/products', (req,res) => {
-  res.json({
-    name: 'turno 1',
-    price: 9
 
-  });
-});
+
+// app.get('/categories/:categoryId/products/:productId', (req, res)=> {
+//   const { categoryId, productId } = req.params;
+//   res.json ({
+//     categoryId,
+//     productId
+//   });
+// });
+// app.get('/users', (req, res) => {
+//   const { limit, offset } = req.query;
+//   if(limit && offset) {
+//     res.json({
+//       limit,
+//       offset
+//     });
+//   }else {
+//     res.send('no hay parametros');
+//   }
+
+// })
+routerApi(app);
 
 app.listen(port, () => {
-  console.log('Mi port ' + port)
-})
+  console.log('Mi port ' + port);
+});
